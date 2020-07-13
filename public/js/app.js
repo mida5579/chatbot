8d044e0e6590d37ca22c22b0478e74ca20304eb6
mael124 <maelbah124@gmail.com>
2020-07-13 18:03:18 +0000

    chatbot interface client

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+// Fiare afficher le bloc au clic 
+var btn = document.getElementById("btn");
+var chatBloc = document.getElementById("chatBloc");
+var btnFermer = document.getElementById("btnFermer");
+
+btn.addEventListener('click', function () {
+    chatBloc.classList.toggle('chatBlocView');
+});
+
+btnFermer.addEventListener('click', function(){
+    chatBloc.classList.remove("chatBlocView");
+});
